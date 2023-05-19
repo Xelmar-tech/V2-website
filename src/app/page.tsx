847041b13289601b588218581bb39948e4c8e64a
IsraelRex <46507005+Israelrex9@users.noreply.github.com>
2023-05-19 06:15:50 +0100

    Icedcool comment and colored logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,14 +27,14 @@ export default function Home() {
         <hr className="max-w-6xl mt-18 w-full"/>
           
         <div className="max-w-6xl w-full space-y-12 py-20">
-              <p className="font-work text-center text-[#B5B4B4] text-lg leading-7">AS A TEAM, WE COLLABORATE WITH LEADING WEB3 PROJECTS AND DAOS TO <span className="text-black">STRATEGIZE (UX)</span> AND <span className="text-black">DESIGN (UI)</span> <br/>
+            <p className="font-work text-center text-[#B5B4B4] text-lg leading-7">AS A TEAM, WE COLLABORATE WITH LEADING WEB3 PROJECTS AND DAOS TO <span className="text-black">STRATEGIZE (UX)</span> AND <span className="text-black">DESIGN (UI)</span> <br/>
                 FOR INNOVATIVE PRODUCTS THAT ARE <span className="text-black">INTUITIVE, ENGAGING</span> AND <span className="text-black">FAMILIAR.</span></p>
-              <div className="grid md:grid-cols-4 grid-cols-2 gap-8 justify-between">
+            <div className="grid md:grid-cols-4 grid-cols-2 gap-8 justify-between">
                 <Image src="/image/Bankless Loans.png" width={238.5} height={40} alt="Bankless Loans"/>
                 <Image src="/image/White Hat DAO.png" width={226} height={40} alt="White Hat DAO"/>
                 <Image src="/image/Vitareels.png" width={151.56} height={40} alt="Vitareels"/>
                 <Image src="/image/TalentDAO 1.png" width={169} height={40} alt="TalentDAO"/>
-              </div>
+            </div>
         </div>
 
         <hr className="max-w-6xl w-full"/>
@@ -48,14 +48,14 @@ export default function Home() {
             <h2 className="font-syne text-left">Design Systems</h2>
             <h2 className="font-syne text-left">Usability Test</h2>
           </div>
-          <a>
+          <a href="https://dribbble.com/IsraelRex" target="blank">
             <div className="overflow-hidden rounded-2xl">
                 
-              {/* <Image alt='img' src="/image/image_02.png" width={555} height={557} className=" h-auto max-w-full hover:scale-125 transition-all duration-500 cursor-pointer"/>
+              {/* <Image alt='img' src="/image/image_02.png" width={555} height={557} className=" h-auto max-w-full hover:scale-125 transition-all duration-500 cursor-pointer"/> */}
                   
-              <Image alt='img' src="/image/image_01.png" width={555} height={557} className=" h-auto max-w-full hover:scale-125 transition-all duration-500 cursor-pointer"/> */}
+              <Image alt='img' src="/image/image_01.png" width={555} height={557} className=" h-auto max-w-full hover:scale-125 transition-all duration-500 cursor-pointer"/>
                     
-              <Image alt='img' src="/image/image_03.png" width={555} height={557} className=" h-auto max-w-full hover:scale-125 transition-all duration-500 cursor-pointer"/>
+              {/* <Image alt='img' src="/image/image_03.png" width={555} height={557} className=" h-auto max-w-full hover:scale-125 transition-all duration-500 cursor-pointer"/> */}
                 
             </div>
           </a>
@@ -66,14 +66,16 @@ export default function Home() {
         <div className="py-16 max-w-6xl w-full justify-between flex md:flex-row flex-col gap-12">
           <div className="max-w-lg flex flex-col gap-7">
                   <h3 className="font-syne text-[#D4D4D4] text-3xl font-medium leading-[3.06rem]">
-                      Working with Rex was a great experience. He's a Product Designer that goes overboard to understand the vision and
-                      translates it into mind-blowing designs.
+                  IsraelRex is top notch!
+                  <br/>I trust his ability to rapidly comprehend and transform wireframes, mockups, and ideas into high-quality designs.
+                  He consistently delivers professional designs that facilitate clear and straightforward implementation for developers.
+                  Furthermore, his enthusiastic and cooperative attitude enhances the overall experience of working with him, making him an excellent designer.
                   </h3>
                   <div className="flex items-center gap-4">
-                      <Image src="/image/Nathaniel Luz.png" width={57} height={57} alt="Nathaniel Luz"/>
+                      <Image src="/image/icedcool.png" width={57} height={57} alt="icedcool"/>
                       <div className="flex flex-col text-black">
-                          <p className="font-syne font-bold text-xl">Nathaniel Luz</p>
-                          <p className="font=syne font-medium text-sm">CEO at Flincap</p>
+                          <p className="font-syne font-bold text-xl">Icedcool</p>
+                          <p className="font=syne font-medium text-sm">Project Manager at BC</p>
                       </div>
                   </div>
           </div>
@@ -81,7 +83,7 @@ export default function Home() {
             <h3 className="font-syne text-[#D4D4D4] text-3xl font-medium leading-[3.06rem]">
                   IsraelRex did an exceptional job designing the Bankless Loans Frontend for Liquity Protocol.
                   I was impressed with his ability to seamlessly integrate our instructions with his own innovative
-                  ideas and improvements. IsraelRex demonstrated a keen sense of aesthetics and a contemporary design
+                  ideas and improvements. He demonstrated a keen sense of aesthetics and a contemporary design
                   style that is sure to resonate with users. I appreciated his timely delivery. Overall, I highly
                   recommend IsraelRex for any design project.
             </h3>
@@ -116,7 +118,7 @@ export default function Home() {
                   XELMAR IS A PRODUCT STUDIO, SPECIALIZED IN BUILDING USER INTERFACES (UI) FOR DECENTRALIZED APPLICATIONS (dAPP) AND OTHER WEB3 TECHNOLOGIES.
                   WE HELP STARTUPS, BUSINESSES AND ORGANIZATIONS CREATE SEAMLESS AND USER-FRIENDLY EXPERIENCES THAT LEVERAGE THE POWER OF BLOCKCHAIN AND OTHER DECENTRALIZED TECHNOLOGIES.
               </p>
-              <div className="max-w-6xl w-full font-syne bg-[#2AD3D5]">
+              <div className="max-w-6xl w-full font-syne bg-[#2AD3D5] cursor-pointer">
                   <h3 className="text-white text-center py-16 font-bold text-4xl leading-[3.75rem] hover:bg-gradient-to-r hover:from-[#FAEF41] hover:to-[#2AD3D4]">
                       Work with us
                   </h3>
@@ -126,6 +128,7 @@ export default function Home() {
                   <div className="flex gap-5">
                       <a className="cursor-pointer" href="https://www.linkedin.com/company/xelmartech/" target="blank">LINKEDIN</a>
                       <a className="cursor-pointer" href="https://twitter.com/xelmartech?s=21&t=u5tJLmhWHkGcx1F2CYdixA" target="blank">TWITTER</a>
+                      <a className="cursor-pointer" href="https://dribbble.com/IsraelRex" target="blank">DRIBBBLE</a>
                   </div>
               </div>
           </div>
